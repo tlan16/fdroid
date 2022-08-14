5eade9066430046d82096c4446403d5a8fba0479
Frank Lan <frank.lan@rea-group.com>
2022-08-14 18:14:41 +1000

    baidu and bromite webview

--- download-apks.sh
+++ download-apks.sh
@@ -5,12 +5,27 @@ BASEDIR=$(realpath "$(dirname "$0")")
 echo "BASEDIR: $BASEDIR"
 mkdir -p "$BASEDIR"/fdroid/repo
 
+getGrepBin() {
+#  use ggrep if available
+  if which ggrep >/dev/null 2>&1; then
+    echo "ggrep"
+  else
+    echo "grep"
+  fi
+}
+
 getReleaseUrl() {
   local owner repo apiUrl
   owner="$1"
   repo="$2"
   apiUrl="https://api.github.com/repos/$owner/$repo/releases/latest"
   curl -s "$apiUrl" | grep browser_ | cut -d\" -f4
+}
+
+getBromiteWebViewUrl() {
+  local owner repo apiUrl
+  apiUrl="https://api.github.com/repos/bromite/bromite/releases/latest"
+  curl -s "$apiUrl" | "$(getGrepBin)" --only-matching --perl-regexp 'https:\/\/.+arm64_SystemWebView\.apk'
 }
 
 # Prepare URLs
@@ -44,6 +59,12 @@ echo "ZhiLiaoUrl: $ZhiLiaoUrl"
 BilibiliChinaUrl="https://dl.hdslb.com/mobile/latest/android64/iBiliPlayer-bili.apk"
 echo "BilibiliChinaUrl: $BilibiliChinaUrl"
 
+BaiduUrl="https://www.wandoujia.com/apps/39899/download/dot"
+echo "BaiduUrl: $BaiduUrl"
+
+BromiteWebViewUrl=$(getBromiteWebViewUrl)
+echo "BromiteWebViewUrl: $BromiteWebViewUrl"
+
 # Download
 parallel \
   --jobs 8 \
@@ -59,7 +80,9 @@ parallel \
   "curl -L $WechatXUrl -o fdroid/repo/WechatX.apk" \
   "curl -L $XposedSmsCodeUrl -o fdroid/repo/Xposed_SMS_Code.apk" \
   "curl -L $ZhiLiaoUrl -o fdroid/repo/Zhi_Liao.apk" \
-  "curl -L $BilibiliChinaUrl -o fdroid/repo/Bilibili_China.apk"
+  "curl -L $BilibiliChinaUrl -o fdroid/repo/Bilibili_China.apk" \
+  "curl -L $BaiduUrl -o fdroid/repo/Baidu.apk" \
+  "curl -L $BromiteWebViewUrl -o fdroid/repo/Bromite_WebView.apk"
 
 # Report
 sleep 0.1 # Sleep to allow parallel to finish
