ceeeb6d2581d8db0aaa98580b8f499ca1554c3a5
Frank Lan <franklan118@gmail.com>
2022-11-12 09:34:19 +1100

    silence curl

--- download-apks.sh
+++ download-apks.sh
@@ -19,13 +19,13 @@ getReleaseUrl() {
   owner="$1"
   repo="$2"
   apiUrl="https://api.github.com/repos/$owner/$repo/releases/latest"
-  curl -s "$apiUrl" | grep browser_ | cut -d\" -f4
+  curl --silent "$apiUrl" | grep browser_ | cut -d\" -f4
 }
 
 getBromiteWebViewUrl() {
   local owner repo apiUrl
   apiUrl="https://api.github.com/repos/bromite/bromite/releases/latest"
-  curl -s "$apiUrl" | "$(getGrepBin)" --only-matching --perl-regexp 'https:\/\/.+arm64_SystemWebView\.apk'
+  curl --silent "$apiUrl" | "$(getGrepBin)" --only-matching --perl-regexp 'https:\/\/.+arm64_SystemWebView\.apk'
 }
 
 # Prepare URLs
@@ -80,21 +80,21 @@ parallel \
   --keep-order \
   --line-buffer \
   sh -c ::: \
-  "curl -L $VancedMicroGUrl -o fdroid/repo/Vanced_MicroG.apk" \
-  "curl -L $RevancedNonRootUrl -o fdroid/repo/Revanced_Nonroot.apk" \
-  "curl -L $FairMailUrl -o fdroid/repo/Fair_Mail.apk" \
-  "curl -L $AnyWebViewUrl -o fdroid/repo/Any_Web_View.apk" \
-  "curl -L $AppSettingsRebornUrl -o fdroid/repo/App_Settings_Reborn.apk" \
-  "curl -L $PixelifyGooglePhotosUrl -o fdroid/repo/Pixelify_Google_Photos.apk" \
-  "curl -L $WechatXUrl -o fdroid/repo/WechatX.apk" \
-  "curl -L $XposedSmsCodeUrl -o fdroid/repo/Xposed_SMS_Code.apk" \
-  "curl -L $BiliRoamingUrl -o fdroid/repo/Bili_Roaming.apk" \
-  "curl -L $ZhiLiaoUrl -o fdroid/repo/Zhi_Liao.apk" \
-  "curl -L $BilibiliChinaUrl -o fdroid/repo/Bilibili_China.apk" \
-  "curl -L $BaiduUrl -o fdroid/repo/Baidu.apk" \
-  "curl -L $BromiteWebViewUrl -o fdroid/repo/Bromite_WebView.apk" \
-  "curl -L $ChineseConsulateUrl -o fdroid/repo/Chinese_Consulate.apk" \
-  "curl -L $ALHZUrl -o fdroid/repo/alhz.apk"
+  "curl --silent --location $VancedMicroGUrl -o fdroid/repo/Vanced_MicroG.apk" \
+  "curl --silent --location $RevancedNonRootUrl -o fdroid/repo/Revanced_Nonroot.apk" \
+  "curl --silent --location $FairMailUrl -o fdroid/repo/Fair_Mail.apk" \
+  "curl --silent --location $AnyWebViewUrl -o fdroid/repo/Any_Web_View.apk" \
+  "curl --silent --location $AppSettingsRebornUrl -o fdroid/repo/App_Settings_Reborn.apk" \
+  "curl --silent --location $PixelifyGooglePhotosUrl -o fdroid/repo/Pixelify_Google_Photos.apk" \
+  "curl --silent --location $WechatXUrl -o fdroid/repo/WechatX.apk" \
+  "curl --silent --location $XposedSmsCodeUrl -o fdroid/repo/Xposed_SMS_Code.apk" \
+  "curl --silent --location $BiliRoamingUrl -o fdroid/repo/Bili_Roaming.apk" \
+  "curl --silent --location $ZhiLiaoUrl -o fdroid/repo/Zhi_Liao.apk" \
+  "curl --silent --location $BilibiliChinaUrl -o fdroid/repo/Bilibili_China.apk" \
+  "curl --silent --location $BaiduUrl -o fdroid/repo/Baidu.apk" \
+  "curl --silent --location $BromiteWebViewUrl -o fdroid/repo/Bromite_WebView.apk" \
+  "curl --silent --location $ChineseConsulateUrl -o fdroid/repo/Chinese_Consulate.apk" \
+  "curl --silent --location $ALHZUrl -o fdroid/repo/alhz.apk"
 
 # Report
 sleep 0.1 # Sleep to allow parallel to finish
