7a8d30483e3c1fed5a95c83e5eb73fd832b2e538
Frank Lan <frank.lan@rea-group.com>
2025-02-14 11:27:09 +1100

    add apk validation

--- validate-apks.sh
+++ validate-apks.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+cd "$(dirname "$0")" || exit 1
+docker compose pull fdroid
+cd "$(dirname "$0")/fdroid" || exit 1
+
 function try_validate_zip() {
   local apk
   apk="$1"
   echo "Validating $apk"
   unzip -t -qq "$apk"
+  docker compose run --rm --remove-orphans --entrypoint 'uv' fdroid run androguard arsc "$apk" > /dev/null
 }
 
-function try_validate_resources_arsc() {
-  local apk
-  apk="$1"
-  echo "Validating resources.arsc in $apk"
-  unzip -Z1 "$apk" | grep -qw 'resources.arsc'
-}
-
-for apk in ./fdroid/repo/*.apk; do
+for apk in ./repo/*.apk; do
   # Delete the file if it's not a valid zip file
   if ! try_validate_zip "$apk"; then
     rm -vf "$apk"
   fi
 done
+
+cd "$(dirname "$0")" || exit 1
